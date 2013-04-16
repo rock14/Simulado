ffe3bece9f5e18a50bba1771f1f8b2f21ef38dbd
Dyeison Schreiner <dyeison.kut@gmail.com>
2013-04-16 15:30:48 -0300

    gt

---
+++ cadastro.php
@@ -0,0 +1,16 @@
+<?php
+	include( 'index.html' );
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$senha = ($_POST['senha']);
+
+	$con = mysql_pconnect("XwGo6Y94p8Sm.db.10856794.hostedresource.com", "XwGo6Y94p8Sm", "as@quA00P7JcC") or die("Could not connect");
+	mysql_select_db("XwGo6Y94p8Sm") or die("Could not select database");
+	
+	$query = "INSERT INTO usuario (nome,email,senha) VALUES ('$nome','$email','$senha')";
+    $insert = mysql_query($query,$con);
+	
+	mysqli_close($con);
+	
+?>
+
